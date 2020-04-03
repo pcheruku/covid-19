933b21bb219e7eafa5eff667861dd878bf568405
Praveen Cheruku <praveen.cheruku@eyefinity.com>
2020-04-03 12:40:22 -0700

    Added css, js

---
+++ scripts/utilcode.js
@@ -0,0 +1,122 @@
+function getCountryData() {
+	var htmlData = "<div class='row header'><div class='cell'>Country</div><div class='cell'>Positive</div><div class='cell'>Negative</div><div class='cell'>Pending</div><div class='cell'>Total</div><div class='cell'>Deaths</div><div class='cell'>Last Modified (PST)</div></div>";
+	$.getJSON('https://covidtracking.com/api/us', function(data) {
+		$.each(data, function(key, value){
+			htmlData += "<div class='row'>";
+			htmlData += "<div class='cell' data-title='Country'>United States (USA)</div>";
+			htmlData += parseData(value);
+			htmlData += "<div class='cell' data-title='Last Modified'>" + (new Date( Date.parse(value.lastModified))).toLocaleString() + "</div>";
+			htmlData += "</div>";										
+		});
+		document.getElementById('divTable').innerHTML = htmlData;				
+	});						
+}
+
+function getStateData() {
+	var htmlStateData = "<div class='row header'><div class='cell'>State</div><div class='cell'>Positive</div><div class='cell'>Negative</div><div class='cell'>Pending</div><div class='cell'>Total</div><div class='cell'>Deaths</div><div class='cell'>Last Modified (PST)</div></div>";
+	$.getJSON('https://covidtracking.com/api/states', function(data) {
+		$.each(data, function(key, value){
+			htmlStateData += "<div class='row'>";
+			htmlStateData += "<div class='cell' data-title='State'>" + abbrState(value.state, 'name') + "</div>";
+			htmlStateData += parseData(value);
+			htmlStateData += "<div class='cell' data-title='Last Modified'>" + (new Date( Date.parse(value.dateModified))).toLocaleString() + "</div>";					
+			htmlStateData += "</div>";					
+		});
+		document.getElementById('divStateTable').innerHTML = htmlStateData;				
+	});						
+}
+
+function parseData(value){
+	var data = "<div class='cell' data-title='Positive'>" + getValue(value.positive) + "</div>";
+	data += "<div class='cell' data-title='Negative'>" + getValue(value.negative) + "</div>";
+	data += "<div class='cell' data-title='Pending'>" + getValue(value.pending) + "</div>";
+	data += "<div class='cell' data-title='Total'>" + getValue(value.total) + "</div>";
+	data += "<div class='cell' data-title='Deaths'>" + getValue(value.death) + "</div>";
+	return data;
+}
+
+function getValue(val){
+	if(val == null) {
+		return 0;
+	}
+	else {
+		return val;
+	}			
+}		
+
+function abbrState(input, to){			
+	var states = [
+		['Arizona', 'AZ'],
+		['Alabama', 'AL'],
+		['Alaska', 'AK'],
+		['Arkansas', 'AR'],
+		['California', 'CA'],
+		['Colorado', 'CO'],
+		['Connecticut', 'CT'],
+		['Delaware', 'DE'],
+		['Florida', 'FL'],
+		['Georgia', 'GA'],
+		['Hawaii', 'HI'],
+		['Idaho', 'ID'],
+		['Illinois', 'IL'],
+		['Indiana', 'IN'],
+		['Iowa', 'IA'],
+		['Kansas', 'KS'],
+		['Kentucky', 'KY'],
+		['Louisiana', 'LA'],
+		['Maine', 'ME'],
+		['Maryland', 'MD'],
+		['Massachusetts', 'MA'],
+		['Michigan', 'MI'],
+		['Minnesota', 'MN'],
+		['Mississippi', 'MS'],
+		['Missouri', 'MO'],
+		['Montana', 'MT'],
+		['Nebraska', 'NE'],
+		['Nevada', 'NV'],
+		['New Hampshire', 'NH'],
+		['New Jersey', 'NJ'],
+		['New Mexico', 'NM'],
+		['New York', 'NY'],
+		['North Carolina', 'NC'],
+		['North Dakota', 'ND'],
+		['Ohio', 'OH'],
+		['Oklahoma', 'OK'],
+		['Oregon', 'OR'],
+		['Pennsylvania', 'PA'],
+		['Rhode Island', 'RI'],
+		['South Carolina', 'SC'],
+		['South Dakota', 'SD'],
+		['Tennessee', 'TN'],
+		['Texas', 'TX'],
+		['Utah', 'UT'],
+		['Vermont', 'VT'],
+		['Virginia', 'VA'],
+		['Washington', 'WA'],
+		['West Virginia', 'WV'],
+		['Wisconsin', 'WI'],
+		['Wyoming', 'WY'],
+		['American Samoa', 'AS'],
+		['District Of Columbia', 'DC'],
+		['Guam', 'GU'],
+		['N Mariana Islands', 'MP'],
+		['Puerto Rico', 'PR'],
+		['US Virgin Islands', 'VI'],
+	];
+
+	if (to == 'abbr'){
+		input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+		for(i = 0; i < states.length; i++){
+			if(states[i][0] == input){
+				return(states[i][1]);
+			}
+		}    
+	} else if (to == 'name'){
+		input = input.toUpperCase();
+		for(i = 0; i < states.length; i++){
+			if(states[i][1] == input){
+				return(states[i][0]);
+			}
+		}    
+	}
+}
